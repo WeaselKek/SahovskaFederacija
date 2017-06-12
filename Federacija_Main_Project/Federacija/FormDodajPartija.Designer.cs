b88736ee2f939a5f319b397350bebd71980e0fa1
WeaselKek <pilgrimdestroyer@gmail.com>
2017-06-12 21:01:22 +0200

    Party all day

--- Federacija_Main_Project/Federacija/FormDodajPartija.Designer.cs
+++ Federacija_Main_Project/Federacija/FormDodajPartija.Designer.cs
@@ -32,14 +32,15 @@
             this.btnPotvrdi = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dtpDat = new System.Windows.Forms.DateTimePicker();
+            this.txtPartVreme = new System.Windows.Forms.TextBox();
+            this.txtPartTrajanje = new System.Windows.Forms.TextBox();
             this.grpIshod = new System.Windows.Forms.GroupBox();
             this.rdbCrni = new System.Windows.Forms.RadioButton();
             this.rdbRemi = new System.Windows.Forms.RadioButton();
             this.rdbBeli = new System.Windows.Forms.RadioButton();
             this.pnlPotez = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
             this.lstPotez = new System.Windows.Forms.ListBox();
             this.btnObrisiPotez = new System.Windows.Forms.Button();
             this.btnIzmeniPotez = new System.Windows.Forms.Button();
@@ -54,7 +55,6 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.dgvSahista = new System.Windows.Forms.DataGridView();
             this.btnBeli = new System.Windows.Forms.Button();
             this.btnCrni = new System.Windows.Forms.Button();
@@ -62,9 +62,15 @@
             this.label11 = new System.Windows.Forms.Label();
             this.lblBeli = new System.Windows.Forms.Label();
             this.lblCrni = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvSudija = new System.Windows.Forms.DataGridView();
+            this.lblSudija = new System.Windows.Forms.Label();
+            this.btnSudija = new System.Windows.Forms.Button();
             this.grpIshod.SuspendLayout();
             this.pnlPotez.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSahista)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSudija)).BeginInit();
             this.SuspendLayout();
             // 
             // btnCancel
@@ -85,6 +91,7 @@
             this.btnPotvrdi.TabIndex = 1;
             this.btnPotvrdi.Text = "Potvrdi";
             this.btnPotvrdi.UseVisualStyleBackColor = true;
+            this.btnPotvrdi.Click += new System.EventHandler(this.btnPotvrdi_Click);
             // 
             // label1
             // 
@@ -106,36 +113,36 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "label2";
             // 
-            // dateTimePicker1
+            // dtpDat
             // 
-            this.dateTimePicker1.CustomFormat = "dd.MM.yyyy";
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(47, 101);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(149, 26);
-            this.dateTimePicker1.TabIndex = 4;
+            this.dtpDat.CustomFormat = "dd.MM.yyyy";
+            this.dtpDat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpDat.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDat.Location = new System.Drawing.Point(34, 61);
+            this.dtpDat.Name = "dtpDat";
+            this.dtpDat.Size = new System.Drawing.Size(149, 26);
+            this.dtpDat.TabIndex = 4;
             // 
-            // textBox1
+            // txtPartVreme
             // 
-            this.textBox1.Location = new System.Drawing.Point(47, 146);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(149, 20);
-            this.textBox1.TabIndex = 5;
+            this.txtPartVreme.Location = new System.Drawing.Point(34, 106);
+            this.txtPartVreme.Name = "txtPartVreme";
+            this.txtPartVreme.Size = new System.Drawing.Size(149, 20);
+            this.txtPartVreme.TabIndex = 5;
             // 
-            // textBox2
+            // txtPartTrajanje
             // 
-            this.textBox2.Location = new System.Drawing.Point(47, 196);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(149, 20);
-            this.textBox2.TabIndex = 6;
+            this.txtPartTrajanje.Location = new System.Drawing.Point(34, 156);
+            this.txtPartTrajanje.Name = "txtPartTrajanje";
+            this.txtPartTrajanje.Size = new System.Drawing.Size(149, 20);
+            this.txtPartTrajanje.TabIndex = 6;
             // 
             // grpIshod
             // 
             this.grpIshod.Controls.Add(this.rdbCrni);
             this.grpIshod.Controls.Add(this.rdbRemi);
             this.grpIshod.Controls.Add(this.rdbBeli);
-            this.grpIshod.Location = new System.Drawing.Point(50, 236);
+            this.grpIshod.Location = new System.Drawing.Point(37, 196);
             this.grpIshod.Name = "grpIshod";
             this.grpIshod.Size = new System.Drawing.Size(110, 109);
             this.grpIshod.TabIndex = 7;
@@ -193,6 +200,15 @@
             this.pnlPotez.Name = "pnlPotez";
             this.pnlPotez.Size = new System.Drawing.Size(477, 271);
             this.pnlPotez.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(14, 17);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Redni Broj:";
             // 
             // lstPotez
             // 
@@ -297,7 +313,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(53, 87);
+            this.label6.Location = new System.Drawing.Point(40, 47);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(38, 13);
             this.label6.TabIndex = 9;
@@ -306,7 +322,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(47, 130);
+            this.label7.Location = new System.Drawing.Point(34, 90);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(37, 13);
             this.label7.TabIndex = 10;
@@ -315,20 +331,11 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(47, 171);
+            this.label8.Location = new System.Drawing.Point(34, 131);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(45, 13);
             this.label8.TabIndex = 11;
             this.label8.Text = "Trajanje";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(14, 17);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(59, 13);
-            this.label9.TabIndex = 11;
-            this.label9.Text = "Redni Broj:";
             // 
             // dgvSahista
             // 
@@ -395,11 +402,51 @@
             this.lblCrni.TabIndex = 18;
             this.lblCrni.Text = "Nije izabran";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(270, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(8, 8);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // dgvSudija
+            // 
+            this.dgvSudija.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvSudija.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSudija.Location = new System.Drawing.Point(34, 326);
+            this.dgvSudija.Name = "dgvSudija";
+            this.dgvSudija.Size = new System.Drawing.Size(405, 153);
+            this.dgvSudija.TabIndex = 20;
+            // 
+            // lblSudija
+            // 
+            this.lblSudija.AutoSize = true;
+            this.lblSudija.Location = new System.Drawing.Point(40, 528);
+            this.lblSudija.Name = "lblSudija";
+            this.lblSudija.Size = new System.Drawing.Size(63, 13);
+            this.lblSudija.TabIndex = 21;
+            this.lblSudija.Text = "Nije Izabran";
+            // 
+            // btnSudija
+            // 
+            this.btnSudija.Location = new System.Drawing.Point(48, 496);
+            this.btnSudija.Name = "btnSudija";
+            this.btnSudija.Size = new System.Drawing.Size(75, 23);
+            this.btnSudija.TabIndex = 22;
+            this.btnSudija.Text = "Dodaj Sudiju";
+            this.btnSudija.UseVisualStyleBackColor = true;
+            this.btnSudija.Click += new System.EventHandler(this.btnSudija_Click);
+            // 
             // FormDodajPartija
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(992, 638);
+            this.Controls.Add(this.btnSudija);
+            this.Controls.Add(this.lblSudija);
+            this.Controls.Add(this.dgvSudija);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.lblCrni);
             this.Controls.Add(this.lblBeli);
             this.Controls.Add(this.label11);
@@ -412,9 +459,9 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.pnlPotez);
             this.Controls.Add(this.grpIshod);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtPartTrajanje);
+            this.Controls.Add(this.txtPartVreme);
+            this.Controls.Add(this.dtpDat);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnPotvrdi);
@@ -428,6 +475,8 @@
             this.pnlPotez.ResumeLayout(false);
             this.pnlPotez.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSahista)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSudija)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -439,9 +488,9 @@
         private System.Windows.Forms.Button btnPotvrdi;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DateTimePicker dtpDat;
+        private System.Windows.Forms.TextBox txtPartVreme;
+        private System.Windows.Forms.TextBox txtPartTrajanje;
         private System.Windows.Forms.GroupBox grpIshod;
         private System.Windows.Forms.RadioButton rdbCrni;
         private System.Windows.Forms.RadioButton rdbRemi;
@@ -469,5 +518,9 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label lblBeli;
         private System.Windows.Forms.Label lblCrni;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvSudija;
+        private System.Windows.Forms.Label lblSudija;
+        private System.Windows.Forms.Button btnSudija;
     }
 }
--- Federacija_Main_Project/Federacija/FormDodajPartija.cs
+++ Federacija_Main_Project/Federacija/FormDodajPartija.cs
@@ -34,9 +34,10 @@ namespace Federacija
             get;
             set;
         }
-        Sahista Beli = null;
-        Sahista Crni = null;
+        Sahista Beli;
+        Sahista Crni;
         IList<Potez> Ptz;
+        Sudija Sudac;
         private void FormDodajPartija_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (closenow)
@@ -58,6 +59,7 @@ namespace Federacija
         private void FormDodajPartija_Load(object sender, EventArgs e)
         {
             Ptz = new List<Potez>();
+            rdbBeli.Checked = true;
             label2.Text = Turn.Naziv + "  " + Turn.Godina.ToString() + "  " + Turn.Grad;
             lblRbr.Text = (Ptz.Count() + 1).ToString();
             ucitajDGV();
@@ -66,32 +68,85 @@ namespace Federacija
 
         private void ucitajDGV()
         {
-
-            dgvSahista.Columns.Clear();
-            ISession s = DataLayer.GetSession();
-
-            IList<Sahista> shl = new List<Sahista>();
-
-
-            shl = (from o in s.Query<Sahista>()
-                             select o).ToList<Sahista>();
-
-
-            SortableBindingList<Sahista> a = new SortableBindingList<Sahista>(shl);
-
-            s.Close();
-
-            dgvSahista.DataSource = a;
-
-            //dgv1.AutoGenerateColumns = false;
-            dgvSahista.AllowUserToAddRows = false;
-            foreach (DataGridViewColumn v in dgvSahista.Columns)
+            try
             {
-                v.Visible = false;
+                dgvSahista.Columns.Clear();
+                ISession s = DataLayer.GetSession();
+
+                int i = 0;
+                Organizator or;
+                Majstor m;
+                IList<Sudija> sud = new List<Sudija>();
+
+                sud = (from o in s.Query<Sudija>()
+                       select o).ToList<Sudija>();
+
+
+                SortableBindingList<Sudija> a1 = new SortableBindingList<Sudija>(sud);
+
+
+                IList<Sahista> shl = new List<Sahista>();
+
+                shl = (from o in s.Query<Sahista>()
+                       select o).ToList<Sahista>();
+
+
+                SortableBindingList<Sahista> a2 = new SortableBindingList<Sahista>(shl);
+
+                //s.Close();
+
+                dgvSudija.DataSource=a1;
+                dgvSahista.DataSource = a2;
+
+                //dgvSahista kod
+                dgvSahista.AllowUserToAddRows = false;
+                foreach (DataGridViewColumn v in dgvSahista.Columns)
+                {
+                    v.Visible = false;
+                }
+                dgvSahista.Columns["Ime"].Visible = true;
+                dgvSahista.Columns["Prezime"].Visible = true;
+                dgvSahista.Columns["Drzava"].Visible = true;
+
+                //dgvSudija kod
+                dgvSudija.AllowUserToAddRows = false;
+                dgvSudija.Columns["Id"].Visible = false;
+                dgvSudija.Columns["FlagMajstor"].Visible = false;
+                dgvSudija.Columns["FlagOrganizator"].Visible = false;
+                dgvSudija.Columns.Add("tip","Tip");
+                dgvSudija.Columns.Add("ime", "Ime");
+                dgvSudija.Columns.Add("prezime", "Prezime");
+              
+               
+                foreach (Sudija value in a1)
+                {
+                    if (value.FlagMajstor == 1)
+                    {
+                        m = (from o in s.Query<Majstor>()
+                             where (o.SudijaId == value)
+                             select o).Single();
+                        dgvSudija.Rows[i].Cells["tip"].Value = "Majstor";
+                        dgvSudija.Rows[i].Cells["ime"].Value = m.Ime;
+                        dgvSudija.Rows[i].Cells["prezime"].Value = m.Prezime;
+                    }
+                    else
+                    {
+                        or = (from o in s.Query<Organizator>()
+                              where (o.SudijaId == value)
+                              select o).Single();
+                        dgvSudija.Rows[i].Cells["tip"].Value = "Organizator";
+                        dgvSudija.Rows[i].Cells["ime"].Value = or.Ime;
+                        dgvSudija.Rows[i].Cells["prezime"].Value = or.Prezime;
+                    }
+                    i++;
+                }
+
+                s.Close();
             }
-            dgvSahista.Columns["Ime"].Visible = true;
-            dgvSahista.Columns["Prezime"].Visible = true;
-            dgvSahista.Columns["Drzava"].Visible = true;
+            catch (Exception ec)
+            {
+                MessageBox.Show(ec.Message);
+            }
         }
 
         private void btnDodajPotez_Click(object sender, EventArgs e)
@@ -159,8 +214,8 @@ namespace Federacija
                 return;
             }
             Sahista item = dgvSahista.SelectedRows[0].DataBoundItem as Sahista;
-            if (Crni != null)
-                if (item.RegBr == Crni.RegBr)
+            
+                if (item == Crni)
                 {
                     MessageBox.Show("Sahista ne moze igrati sa samim sobom");
                     return;
@@ -178,14 +233,74 @@ namespace Federacija
                 return;
             }
             Sahista item = dgvSahista.SelectedRows[0].DataBoundItem as Sahista;
-            if (Beli != null)
-                if (item.RegBr == Beli.RegBr)
+           
+                if (item == Beli)
                 {
                     MessageBox.Show("Sahista ne moze igrati sa samim sobom");
                     return;
                 }
             Crni = item;
             lblCrni.Text = Crni.Ime + " " + Crni.Prezime;
+        }
+
+        private void btnPotvrdi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Beli == null || Crni == null || Sudac == null || txtPartTrajanje.Text == "" || txtPartVreme.Text == "")
+                {
+                    MessageBox.Show("Niste pravilno uneli podatke");
+                    return;
+                }
+
+                ISession s = DataLayer.GetSession();
+
+                Partija p = new Partija();
+
+                int num;
+                p.Datum = dtpDat.Value;
+                p.Vreme = txtPartVreme.Text;
+                if(Int32.TryParse(txtPartTrajanje.Text,out num))
+                    p.Trajanje=num;
+                p.Ishod=grpIshod.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked).Text.ToUpper();
+                p.BeliIgrac = Beli;
+                p.CrniIgrac = Crni;
+                foreach (Potez pt in Ptz)
+                {
+                    pt.Partija = p;
+                } 
+                p.Potezi = Ptz;
+                p.Turnir = Turn;
+                p.Sudija = Sudac;
+
+                s.SaveOrUpdate(p);
+
+                s.Flush();
+                s.Close();
+
+                if (!updaterino)
+                    MessageBox.Show("Uspesno dodata Partija");
+                else
+                    MessageBox.Show("Uspesno izmenjena Partija");
+                closenow = true;
+                this.Close();
+            }
+            catch (Exception ec)
+            {
+                MessageBox.Show(ec.Message);
+            }
+        }
+
+        private void btnSudija_Click(object sender, EventArgs e)
+        {
+            if (dgvSudija.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selektujte Sudiju");
+                return;
+            }
+            Sudija Item = dgvSudija.SelectedRows[0].DataBoundItem as Sudija;
+            lblSudija.Text = dgvSudija.SelectedRows[0].Cells["ime"].Value + "  " + dgvSudija.SelectedRows[0].Cells["prezime"].Value;
+            Sudac = Item;
         }
     }
 }
